948fac47d1da97587ac9c39d46090ea0882c9221
Luisma <perronegro49@gmail.com>
2022-06-21 08:36:18 +0200

    camnbio a heroku

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // host:'https://bullseye.herokuapp.com/api/',
-  host:'http://localhost:8080/api/',
+   host:'https://bullseye1.herokuapp.com/api/',
+  // host:'http://localhost:8080/api/',
   itemsPorPagina: 10
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  host: 'http://localhost:8080/api/',
+  host:'https://bullseye1.herokuapp.com/api/',
+  // host: 'http://localhost:8080/api/',
   itemsPorPagina: 100
 };
 
