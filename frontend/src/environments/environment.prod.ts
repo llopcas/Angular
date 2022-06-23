b4d70d91d04ec2ed097b805b6381d478aac95bf6
Luisma <perronegro49@gmail.com>
2022-06-23 08:32:13 +0200

    depliegue

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // host: 'https://electromarket-api.herokuapp.com/api/',
-  host: 'http://localhost:8080/api/',
+  host: 'https://bullseye1.herokuapp.com/api/',
+  // host: 'http://localhost:8080/api/',
   itemsPorPagina:10
 
 };
