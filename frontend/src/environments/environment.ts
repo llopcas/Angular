afca92a8418be9d9986ae668b526fdca68ce3446
Luisma <perronegro49@gmail.com>
2022-06-27 11:47:10 +0200

    deploy

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
   production: false,
-  // host: 'https://bullseyeapi1.herokuapp.com/api/',
-  host: 'http://localhost:8080/api/',
+  host: 'https://bullseyeapi1.herokuapp.com/api/',
+  // host: 'http://localhost:8080/api/',
 
   itemsPorPagina:10
 };
