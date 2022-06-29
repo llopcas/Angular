ad2186d08cbe509b91b4a3d59e54ea77d4e63d89
Luisma <perronegro49@gmail.com>
2022-06-29 23:32:07 +0200

    correcciones BotonesFormularioSelect

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   host: 'https://bullseyeapi1.herokuapp.com/api/',
-  // host: 'http://localhost:8080/api/',
+  //  host: 'http://localhost:8080/api/',
 
   itemsPorPagina:10
 };
